1ea3d9da0fcc6caf3e9f8118dd59aa31e0570f9e
Phill <philltv.gaming@gmail.com>
2022-07-09 01:06:08 +0200

    push

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>TEST</h3>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue";
+    export default defineComponent({
+        data() {
+            return {
+
+            }
+        }
+    })
+</script>
+
+<style scoped>
+@import '../css/home.css';
+</style>
