842ab7847e5b4b1f7b6926b74f07b0eaaadeded4
Phill <philltv.gaming@gmail.com>
2022-07-13 00:11:06 +0200

    Started popup for player list

---
+++ src/components/CCardSnowflake.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="snowflake">
+    <p class="name">{{ data?.name }}</p>
+    <div class="side">
+        <!-- TODO: create sections for different things yk -->
+    </div>
+    <div class="content">
+        <!-- TODO: fill in those things -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Player } from "../interfaces/iPlayer";
+export default defineComponent({
+  data() {
+    return {};
+  },
+  props: {
+    data: {
+      type: Object,
+      default: undefined,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+@import "../css/ccardsnowflake.css";
+</style>
