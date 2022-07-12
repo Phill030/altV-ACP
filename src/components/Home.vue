bb446362de7bbf88b5a25cf9c594f0e974a1d0b4
Phill <philltv.gaming@gmail.com>
2022-07-13 00:11:44 +0200

    Created panel options

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    data() {
+        return {
+
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
