579083ad0c74150734904d9685e9439b8faf6285
Phill <philltv.gaming@gmail.com>
2022-07-13 00:11:24 +0200

    Create custom sidebar for the panel

--- src/components/CSidebar.vue
+++ src/components/CSidebar.vue
@@ -16,7 +16,7 @@
           v-for="(it, idx) in resources"
           :key="idx"
           :style="active == idx ? dynmicStyle : undynmicStyle"
-          @click="active = idx"
+          @click="[active = idx, $emit('idxchange', idx)]"
         >
           <div class="activeIndicator" v-if="active == idx" />
           <img
@@ -41,36 +41,31 @@ export default defineComponent({
         {
           name: "Home",
           icon: "home_icon",
-          path: "/panel/home",
         },
         {
           name: "Players",
           icon: "players_icon",
-          path: "/panel/players",
         },
         {
           name: "Resources",
           icon: "resources_icon",
-          path: "/panel/resources",
         },
         {
           name: "Discord",
           icon: "discord_icon",
-          path: "/panel/discord",
         },
         {
           name: "Map",
           icon: "map_icon",
-          path: "/panel/map",
         },
         {
           name: "Vehicles",
           icon: "vehicles_icon",
-          path: "/panel/vehicles",
         },
       ],
     };
   },
+  emits: ['idxchange'],
   methods: {
     toggleSidebar() {
       this.collapsed = !this.collapsed;
@@ -88,5 +83,5 @@ export default defineComponent({
 </script>
 
 <style scoped>
-@import "../css/sidebar.css";
+@import "../css/csidebar.css";
 </style>
