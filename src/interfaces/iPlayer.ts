1fed9e4845f841600217863d26a6f4154b54c8c6
Phill <philltv.gaming@gmail.com>
2022-07-14 22:17:49 +0200

    Added more values to player interface

--- src/interfaces/iPlayer.ts
+++ src/interfaces/iPlayer.ts
@@ -2,8 +2,18 @@ export interface Player {
     name?: string;
     id?: number;
     ping?: number;
-    ip?: number;
+    ip?: string;
     hwid?: string;
     scid?: string;
     discordId?: string;
+    joined?: Date;
+    health?: number;
+    armor?: number;
+    position?: Vector3;
+}
+
+export interface Vector3 {
+    x: number;
+    y: number;
+    z: number;
 }
