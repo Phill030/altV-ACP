d2b2d9ec4b7316b14ac692301a9e0f15230586d0
Phill <philltv.gaming@gmail.com>
2022-08-15 22:46:27 +0200

    Added CImgButton component

---
+++ src/components/CImgButton.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    data() {
+        return {};
+    },
+    props: {
+        
+    },
+});
+</script>
+
+<style scoped>
+
+</style>
